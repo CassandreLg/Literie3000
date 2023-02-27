3dde8bb5bcdad936db9a29d5f921edb4bddfe22d
Cassandre Lelong <cassandre.lelong@gmail.com>
2023-02-27 16:31:04 +0100

    Tentative d'ajout de la suppression de produit : raté, oups

---
+++ produit.php
@@ -0,0 +1,73 @@
+<?php
+// Inclure le template header
+include("templates/header.php");
+
+$find = false;
+$data = array("name" => "Produit introuvable");
+if (isset($_GET["id"])) {
+    // Connexion à la base literie3000
+    $dsn = "mysql:host=localhost;dbname=literie3000";
+    $db = new PDO($dsn, "root", "");
+
+
+    // RETROUVER LE PRODUIT SELECTIONNE
+
+    // 1) On prépare la requête SQL avec des paramètres
+    $query = $db->prepare("SELECT * FROM produits WHERE id= :id");
+    // 2) On donne des valeurs à nos paramètres
+    $query->bindParam(":id", $_GET["id"], PDO::PARAM_INT);
+    // 3) On exécute notre requête préalablement préparée
+    $query->execute();
+    $produit = $query->fetch(); // Retourne un tableau associatif de la recette concernée ou false si il n'y a pas de correspondance
+
+
+    if ($produit) {
+        // nous avons trouvé le produit
+        $find = true;
+        $data = $produit;
+    }
+}
+
+
+
+// METHODE POUR SUPPRIMER DE LA BDD
+
+// if (!empty($_POST)) {
+
+//     // Requête de supression de la ligne de ma base de données
+
+//     // Connexion à la base literie3000
+//     $dsn = "mysql:host=localhost;dbname=literie3000";
+//     $db = new PDO($dsn, "root", "");
+
+//     // On prépare la méthode
+//     $suppr = $db->prepare("DELETE FROM `produits`
+//         WHERE `id` = :id");
+
+//     // On exécute la méthode
+//     if ($suppr->execute()) {
+//         // La requête s'est bien déroulée, on affiche un message de confirmation
+//         echo ("Le produit a été supprimé");
+//     }
+// }
+// ?>
+
+
+
+
+<!-- PARTIE AFFICHAGE -->
+
+<h1><?= $data["nom"] ?></h1>
+<?php
+if ($find) {
+?>
+    <img src="<?= $data["photo"] ?>" alt="" class="image-produit">
+    <p><?= $data["prix"] ?> €</p>
+    <p><?= $data["promo"] ?> €</p>
+
+    <form action="" method="post">
+    <input type="submit" value="Supprimer le produit" class="bouton">
+    </form>
+
+<?php
+}
